55aaa0f8360c96fd2cc60c8ddd129de277583d8e
Zatil Imani <zatilimaniahwan@gmail.com>
2021-07-04 22:46:20 +0800

    Update..

--- src/components/Modules/Hiragana/sei-on/parts/aO.vue
+++ src/components/Modules/Hiragana/sei-on/parts/aO.vue
@@ -7,18 +7,29 @@
             <b-card-text>
              <h3 class="syllable">{{ syllable.original }}</h3>
              <h5 class="mt-2"> {{ syllable.romaji }}</h5>
-             <b-button  class="bg-primary mt-3"><em class="mdi mdi-pencil-outline"></em></b-button>
+             <b-button  class="bg-primary mt-3" @click="showModal(syllable.original)"><em class="mdi mdi-pencil-outline"></em></b-button>
             <b-button  class="bg-success mt-3 ml-2" @click="play(syllable.original)"><em class="mdi mdi-account-tie-voice"></em></b-button>
             </b-card-text>
           </b-card>
         </b-col>
       </b-row>
+      <modal :modalId="'modal-syllable'">
+        <div class="syllable" slot="content">
+           <img
+              class="img-fluid"
+              alt="Responsive image"
+              :src="imgSrc"
+              width="100%"
+            />
+        </div>
+      </modal>
     </div>
   </section>
 </template>
 <script>
+import Modal from '../../../../Modal/Modal'
 export default {
-  name: 'AO',
+  components: { Modal },
   data () {
     return {
       syllabels: [
@@ -42,10 +53,31 @@ export default {
           original: 'お',
           romaji: 'o'
         }
-      ]
+      ],
+      imgSrc: ''
     }
   },
   methods: {
+    showModal (val) {
+      switch (val) {
+      case 'あ':
+        this.imgSrc = '/assets/gif/hiraA-O/a-min.gif'
+        break
+      case 'い':
+        this.imgSrc = '/assets/gif/hiraA-O/i-min.gif'
+        break
+      case 'う':
+        this.imgSrc = '/assets/gif/hiraA-O/u-min.gif'
+        break
+      case 'え':
+        this.imgSrc = '/assets/gif/hiraA-O/e-min.gif'
+        break
+      case 'お':
+        this.imgSrc = '/assets/gif/hiraA-O/o-min.gif'
+        break
+      }
+      this.$bvModal.show('modal-syllable')
+    },
     play (val) {
       switch (val) {
       case 'あ':
