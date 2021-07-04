b6e212948c299cd565633e33a88ec38342ba76f5
Zatil Imani <zatilimaniahwan@gmail.com>
2021-07-04 23:02:08 +0800

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,12 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
-    // 'plugin:vue/strongly-recommended',
     '@vue/standard'
   ],
   parserOptions: {
-    // ecmaVersion: 6,
     parser: 'babel-eslint'
   },
   rules: {
     indent: ['error', 2]
-    /* quotes: ['error', 'single'],
-      "vue/max-attributes-per-line": ['error', {
-        singleline: 4,
-        multiline: 1
-      }],
-      'vue/attributes-order': 'error',
-      "vue/order-in-components": 'error', */
   }
 }
