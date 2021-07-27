746a65510513c28fd9112fd3aabf96ab5b3469da
Zatil Imani <zatilimaniahwan@gmail.com>
2021-07-27 23:56:17 +0800

    ...

--- src/components/Modules/Katakana/sei-on/parts/naNo.vue
+++ src/components/Modules/Katakana/sei-on/parts/naNo.vue
@@ -7,18 +7,33 @@
             <b-card-text>
              <h3 class="syllable">{{ syllable.original }}</h3>
              <h5 class="mt-2 romaji"> {{ syllable.romaji }}</h5>
-             <b-button  class="bg-primary mt-3"><em class="mdi mdi-pencil-outline"></em></b-button>
+             <b-button
+                class="bg-primary mt-3"
+                @click="showModal(syllable.original)"
+                ><em class="mdi mdi-pencil-outline"></em
+              ></b-button>
              <b-button  class="bg-success mt-3 ml-2" @click="play(syllable.original)"><em class="mdi mdi-account-tie-voice"></em></b-button>
             </b-card-text>
           </b-card>
         </b-col>
       </b-row>
+      <modal class="syllable" :modalId="'modal-syllable-kata-nano'">
+        <div slot="content">
+          <img
+            class="img-fluid"
+            alt="Responsive image"
+            :src="imgSrc"
+            width="100%"
+          />
+        </div>
+      </modal>
     </div>
   </section>
 </template>
 <script>
+import Modal from '../../../../Modal/Modal'
 export default {
-  name: 'NaNo',
+  components: { Modal },
   data () {
     return {
       syllabels: [
@@ -42,10 +57,31 @@ export default {
           original: 'ノ',
           romaji: 'no'
         }
-      ]
+      ],
+      imgSrc: ''
     }
   },
   methods: {
+    showModal (val) {
+      switch (val) {
+      case 'ナ':
+        this.imgSrc = '/assets/gif/kataNa-No/na-min.gif'
+        break
+      case 'ニ':
+        this.imgSrc = '/assets/gif/kataNa-No/ni-min.gif'
+        break
+      case 'ヌ':
+        this.imgSrc = '/assets/gif/kataNa-No/nu-min.gif'
+        break
+      case 'ネ':
+        this.imgSrc = '/assets/gif/kataNa-No/ne-min.gif'
+        break
+      case 'ノ':
+        this.imgSrc = '/assets/gif/kataNa-No/no-min.gif'
+        break
+      }
+      this.$bvModal.show('modal-syllable-kata-nano')
+    },
     play (val) {
       switch (val) {
       case 'ナ':
