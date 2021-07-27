bb6fae36e11d4107e96daa1c51a171bdd49bcda1
Zatil Imani <zatilimaniahwan@gmail.com>
2021-07-27 23:44:54 +0800

    ...

--- src/components/Modules/Katakana/sei-on/parts/haHo.vue
+++ src/components/Modules/Katakana/sei-on/parts/haHo.vue
@@ -2,23 +2,47 @@
   <section class="number-area ptb_30">
     <div class="col-sm-12 col-lg-12 text-center">
       <b-row class="mt-2">
-        <b-col class="mt-2" cols="2" v-for="syllable in syllabels" :key="syllable.original">
+        <b-col
+          class="mt-2"
+          cols="2"
+          v-for="syllable in syllabels"
+          :key="syllable.original"
+        >
           <b-card>
             <b-card-text>
-             <h3 class="syllable">{{ syllable.original }}</h3>
-             <h5 class="mt-2 romaji"> {{ syllable.romaji }}</h5>
-             <b-button  class="bg-primary mt-3"><em class="mdi mdi-pencil-outline"></em></b-button>
-           <b-button  class="bg-success mt-3 ml-2" @click="play(syllable.original)"><em class="mdi mdi-account-tie-voice"></em></b-button>
+              <h3 class="syllable">{{ syllable.original }}</h3>
+              <h5 class="mt-2 romaji">{{ syllable.romaji }}</h5>
+              <b-button
+                class="bg-primary mt-3"
+                @click="showModal(syllable.original)"
+                ><em class="mdi mdi-pencil-outline"></em
+              ></b-button>
+              <b-button
+                class="bg-success mt-3 ml-2"
+                @click="play(syllable.original)"
+                ><em class="mdi mdi-account-tie-voice"></em
+              ></b-button>
             </b-card-text>
           </b-card>
         </b-col>
       </b-row>
+      <modal class="syllable" :modalId="'modal-syllable-kata-haho'">
+        <div slot="content">
+          <img
+            class="img-fluid"
+            alt="Responsive image"
+            :src="imgSrc"
+            width="100%"
+          />
+        </div>
+      </modal>
     </div>
   </section>
 </template>
 <script>
+import Modal from '../../../../Modal/Modal'
 export default {
-  name: 'HaHo',
+  components: { Modal },
   data () {
     return {
       syllabels: [
@@ -42,10 +66,31 @@ export default {
           original: 'ホ',
           romaji: 'ho'
         }
-      ]
+      ],
+      imgSrc: ''
     }
   },
   methods: {
+    showModal (val) {
+      switch (val) {
+      case 'ハ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/ha-min.gif'
+        break
+      case 'ヒ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/hi-min.gif'
+        break
+      case 'フ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/fu-min.gif'
+        break
+      case 'ヘ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/he-min.gif'
+        break
+      case 'ホ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/ho-min.gif'
+        break
+      }
+      this.$bvModal.show('modal-syllable-kata-haho')
+    },
     play (val) {
       switch (val) {
       case 'ハ':
