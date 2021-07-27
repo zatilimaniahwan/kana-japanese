9079e4b17f238965ef4f122230c5c36f349b5cca
Zatil Imani <zatilimaniahwan@gmail.com>
2021-07-28 00:00:43 +0800

    ...

--- src/components/Modules/Katakana/sei-on/parts/raRo.vue
+++ src/components/Modules/Katakana/sei-on/parts/raRo.vue
@@ -7,18 +7,33 @@
             <b-card-text>
              <h3 class="syllable">{{ syllable.original }}</h3>
              <h5 class="mt-2 romaji"> {{ syllable.romaji }}</h5>
-             <b-button  class="bg-primary mt-3"><em class="mdi mdi-pencil-outline"></em></b-button>
+             <b-button
+                class="bg-primary mt-3"
+                @click="showModal(syllable.original)"
+                ><em class="mdi mdi-pencil-outline"></em
+              ></b-button>
            <b-button  class="bg-success mt-3 ml-2" @click="play(syllable.original)"><em class="mdi mdi-account-tie-voice"></em></b-button>
             </b-card-text>
           </b-card>
         </b-col>
       </b-row>
+      <modal class="syllable" :modalId="'modal-syllable-kata-raro'">
+        <div slot="content">
+          <img
+            class="img-fluid"
+            alt="Responsive image"
+            :src="imgSrc"
+            width="100%"
+          />
+        </div>
+      </modal>
     </div>
   </section>
 </template>
 <script>
+import Modal from '../../../../Modal/Modal'
 export default {
-  name: 'RaRo',
+  components: { Modal },
   data () {
     return {
       syllabels: [
@@ -42,10 +57,31 @@ export default {
           original: 'ロ',
           romaji: 'ro'
         }
-      ]
+      ],
+      imgSrc: ''
     }
   },
   methods: {
+    showModal (val) {
+      switch (val) {
+      case 'ラ':
+        this.imgSrc = '/assets/gif/kataRa-Ro/ra-min.gif'
+        break
+      case 'リ':
+        this.imgSrc = '/assets/gif/kataRa-Ro/ri-min.gif'
+        break
+      case 'ル':
+        this.imgSrc = '/assets/gif/kataRa-Ro/ru-min.gif'
+        break
+      case 'レ':
+        this.imgSrc = '/assets/gif/kataRa-Ro/re-min.gif'
+        break
+      case 'ロ':
+        this.imgSrc = '/assets/gif/kataRa-Ro/ro-min.gif'
+        break
+      }
+      this.$bvModal.show('modal-syllable-kata-raro')
+    },
     play (val) {
       switch (val) {
       case 'ラ':
