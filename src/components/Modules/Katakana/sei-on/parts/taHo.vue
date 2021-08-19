737d0139ac061d4ae0e428ee46e54984e97997e1
Zatil Imani Ahwan <zatilimaniahwan@gmail.com>
2021-08-20 00:15:47 +0800

    Add illustration katakana taHo (#35)

--- src/components/Modules/Katakana/sei-on/parts/taHo.vue
+++ src/components/Modules/Katakana/sei-on/parts/taHo.vue
@@ -1,23 +1,56 @@
 <template>
   <section class="number-area ptb_30">
+    <div class="mt-2 col-sm-6 col-md-12 col-lg-12 mb-3">
+      <b-button class="bg-primary" v-b-modal.modal-illustration-taHo
+        >Vocabulary Illustration</b-button
+      >
+      <modal :modalId="'modal-illustration-taHo'" :size="'xl'">
+        <div slot="content">
+          <b-img
+            src="/assets/img/katakana/sei-on/katakanaTA-HO.jpg"
+            fluid
+            alt="Fluid image"
+          ></b-img>
+          <div v-for="illustration in vocab" :key="illustration.katakana">
+            <b-button
+              class="btn"
+              v-bind:style="illustration.styleButton"
+              @click="play(illustration.katakana)"
+              ><p class="syllable text-white">
+                {{ illustration.katakana }}
+              </p></b-button
+            >
+          </div>
+        </div>
+      </modal>
+    </div>
     <div class="col-sm-12 col-lg-12 text-center">
       <b-row class="mt-2">
-        <b-col class="mt-2" cols="2" v-for="syllable in syllabels" :key="syllable.original">
+        <b-col
+          class="mt-2"
+          cols="2"
+          v-for="syllable in syllabels"
+          :key="syllable.original"
+        >
           <b-card class="mb-2">
             <b-card-text>
-             <h3 class="syllable">{{ syllable.original }}</h3>
-             <h5 class="mt-2 romaji"> {{ syllable.romaji }}</h5>
-             <b-button
+              <h3 class="syllable">{{ syllable.original }}</h3>
+              <h5 class="mt-2 romaji">{{ syllable.romaji }}</h5>
+              <b-button
                 class="bg-primary mt-3"
                 @click="showModal(syllable.original)"
                 ><em class="mdi mdi-pencil-outline"></em
               ></b-button>
-           <b-button  class="bg-success mt-3 ml-2" @click="play(syllable.original)"><em class="mdi mdi-account-tie-voice"></em></b-button>
+              <b-button
+                class="bg-success mt-3 ml-2"
+                @click="play(syllable.original)"
+                ><em class="mdi mdi-account-tie-voice"></em
+              ></b-button>
             </b-card-text>
           </b-card>
         </b-col>
       </b-row>
-     <modal class="syllable" :modalId="'modal-syllable-kata-tato'">
+      <modal class="syllable" :modalId="'modal-syllable-kata-tato'">
         <div slot="content">
           <img
             class="img-fluid"
@@ -98,7 +131,65 @@ export default {
           romaji: 'ho'
         }
       ],
-      imgSrc: ''
+      imgSrc: '',
+      vocab: [
+        {
+          katakana: 'タクシー',
+          styleButton: {
+            position: 'absolute',
+            top: '300px',
+            left: '50px'
+          }
+        },
+        {
+          katakana: 'ノート',
+          styleButton: {
+            position: 'absolute',
+            top: '350px',
+            left: '300px'
+          }
+        },
+        {
+          katakana: 'テキスト',
+          styleButton: {
+            position: 'absolute',
+            top: '500px',
+            left: '350px'
+          }
+        },
+        {
+          katakana: 'スーツ',
+          styleButton: {
+            position: 'absolute',
+            top: '480px',
+            left: '750px'
+          }
+        },
+        {
+          katakana: 'コーヒー',
+          styleButton: {
+            position: 'absolute',
+            top: '350px',
+            left: '850px'
+          }
+        },
+        {
+          katakana: 'スカーフ',
+          styleButton: {
+            position: 'absolute',
+            top: '100px',
+            left: '950px'
+          }
+        },
+        {
+          katakana: 'アナウンサー',
+          styleButton: {
+            position: 'absolute',
+            top: '70px',
+            left: '700px'
+          }
+        }
+      ]
     }
   },
   methods: {
@@ -198,6 +289,27 @@ export default {
         break
       case 'ホ':
         this.soundSrc = '/assets/sounds/sei-on/30.mp3'
+        break
+      case 'タクシー':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_TA-HO_takushii.mp3'
+        break
+      case 'ノート':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_TA-HO_nooto.mp3'
+        break
+      case 'テキスト':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_TA-HO_tekisuto.mp3'
+        break
+      case 'スーツ':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_TA-HO_suutsu.mp3'
+        break
+      case 'コーヒー':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_TA-HO_koohii.mp3'
+        break
+      case 'スカーフ':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_TA-HO_sukaafu.mp3'
+        break
+      case 'アナウンサー':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_TA-HO_anaunsaa.mp3'
         break
       }
       const audio = new Audio(this.soundSrc)
