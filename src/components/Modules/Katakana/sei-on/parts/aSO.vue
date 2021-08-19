cbc7a31678b2176d85d3e90abbb47002732a16bd
Zatil Imani Ahwan <zatilimaniahwan@gmail.com>
2021-08-19 22:29:08 +0800

    fix katakana sei-on (#31)

--- src/components/Modules/Katakana/sei-on/parts/aSO.vue
+++ src/components/Modules/Katakana/sei-on/parts/aSO.vue
@@ -3,28 +3,24 @@
     <div class="col-sm-12 col-lg-12 text-center">
       <b-row class="mt-2">
         <b-col class="mt-2" cols="2" v-for="syllable in syllabels" :key="syllable.original">
-          <b-card>
+          <b-card class="mb-2">
             <b-card-text>
              <h3 class="syllable">{{ syllable.original }}</h3>
              <h5 class="mt-2 romaji"> {{ syllable.romaji }}</h5>
-             <b-button
-                class="bg-primary mt-3"
-                @click="showModal(syllable.original)"
-                ><em class="mdi mdi-pencil-outline"></em
-              ></b-button>
+               <b-button  class="bg-primary mt-3" @click="showModal(syllable.original)"><em class="mdi mdi-pencil-outline"></em></b-button>
             <b-button  class="bg-success mt-3 ml-2" @click="play(syllable.original)"><em class="mdi mdi-account-tie-voice"></em></b-button>
             </b-card-text>
           </b-card>
         </b-col>
       </b-row>
-      <modal :modalId="'modal-syllable-kata-kako'">
-        <div class="syllable" slot="content">
-          <img
-            class="img-fluid"
-            alt="Responsive image"
-            :src="imgSrc"
-            width="100%"
-          />
+      <modal :modalId="'modal-syllable-kata-ao'">
+        <div slot="content">
+           <img
+              class="img-fluid"
+              alt="Responsive image"
+              :src="imgSrc"
+              width="100%"
+            />
         </div>
       </modal>
     </div>
@@ -37,6 +33,26 @@ export default {
   data () {
     return {
       syllabels: [
+        {
+          original: 'ア',
+          romaji: 'a'
+        },
+        {
+          original: 'イ',
+          romaji: 'i'
+        },
+        {
+          original: 'ウ',
+          romaji: 'u'
+        },
+        {
+          original: 'エ',
+          romaji: 'e'
+        },
+        {
+          original: 'オ',
+          romaji: 'o'
+        },
         {
           original: 'カ',
           romaji: 'ka'
@@ -56,6 +72,26 @@ export default {
         {
           original: 'コ',
           romaji: 'ko'
+        },
+        {
+          original: 'サ',
+          romaji: 'sa'
+        },
+        {
+          original: 'シ',
+          romaji: 'shi'
+        },
+        {
+          original: 'ス',
+          romaji: 'su'
+        },
+        {
+          original: 'セ',
+          romaji: 'se'
+        },
+        {
+          original: 'ソ',
+          romaji: 'so'
         }
       ],
       imgSrc: ''
@@ -64,6 +100,21 @@ export default {
   methods: {
     showModal (val) {
       switch (val) {
+      case 'ア':
+        this.imgSrc = '/assets/gif/kataA-O/a-min.gif'
+        break
+      case 'イ':
+        this.imgSrc = '/assets/gif/kataA-O/i-min.gif'
+        break
+      case 'ウ':
+        this.imgSrc = '/assets/gif/kataA-O/u-min.gif'
+        break
+      case 'エ':
+        this.imgSrc = '/assets/gif/kataA-O/e-min.gif'
+        break
+      case 'オ':
+        this.imgSrc = '/assets/gif/kataA-O/o-min.gif'
+        break
       case 'カ':
         this.imgSrc = '/assets/gif/kataKa-Ko/ka-min.gif'
         break
@@ -80,10 +131,25 @@ export default {
         this.imgSrc = '/assets/gif/kataKa-Ko/ko-min.gif'
         break
       }
-      this.$bvModal.show('modal-syllable-kata-kako')
+      this.$bvModal.show('modal-syllable-kata-ao')
     },
     play (val) {
       switch (val) {
+      case 'ア':
+        this.soundSrc = '/assets/sounds/sei-on/1.mp3'
+        break
+      case 'イ':
+        this.soundSrc = '/assets/sounds/sei-on/2.mp3'
+        break
+      case 'ウ':
+        this.soundSrc = '/assets/sounds/sei-on/3.mp3'
+        break
+      case 'エ':
+        this.soundSrc = '/assets/sounds/sei-on/4.mp3'
+        break
+      case 'オ':
+        this.soundSrc = '/assets/sounds/sei-on/5.mp3'
+        break
       case 'カ':
         this.soundSrc = '/assets/sounds/sei-on/6.mp3'
         break
--- src/components/Modules/Katakana/sei-on/parts/taHo.vue
+++ src/components/Modules/Katakana/sei-on/parts/taHo.vue
@@ -3,7 +3,7 @@
     <div class="col-sm-12 col-lg-12 text-center">
       <b-row class="mt-2">
         <b-col class="mt-2" cols="2" v-for="syllable in syllabels" :key="syllable.original">
-          <b-card>
+          <b-card class="mb-2">
             <b-card-text>
              <h3 class="syllable">{{ syllable.original }}</h3>
              <h5 class="mt-2 romaji"> {{ syllable.romaji }}</h5>
@@ -56,6 +56,46 @@ export default {
         {
           original: 'ト',
           romaji: 'to'
+        },
+        {
+          original: 'ナ',
+          romaji: 'na'
+        },
+        {
+          original: 'ニ',
+          romaji: 'ni'
+        },
+        {
+          original: 'ヌ',
+          romaji: 'nu'
+        },
+        {
+          original: 'ネ',
+          romaji: 'ne'
+        },
+        {
+          original: 'ノ',
+          romaji: 'no'
+        },
+        {
+          original: 'ハ',
+          romaji: 'ha'
+        },
+        {
+          original: 'ヒ',
+          romaji: 'hi'
+        },
+        {
+          original: 'フ',
+          romaji: 'fu'
+        },
+        {
+          original: 'ヘ',
+          romaji: 'he'
+        },
+        {
+          original: 'ホ',
+          romaji: 'ho'
         }
       ],
       imgSrc: ''
@@ -79,6 +119,36 @@ export default {
       case 'ト':
         this.imgSrc = '/assets/gif/kataTa-To/to-min.gif'
         break
+      case 'ナ':
+        this.imgSrc = '/assets/gif/kataNa-No/na-min.gif'
+        break
+      case 'ニ':
+        this.imgSrc = '/assets/gif/kataNa-No/ni-min.gif'
+        break
+      case 'ヌ':
+        this.imgSrc = '/assets/gif/kataNa-No/nu-min.gif'
+        break
+      case 'ネ':
+        this.imgSrc = '/assets/gif/kataNa-No/ne-min.gif'
+        break
+      case 'ノ':
+        this.imgSrc = '/assets/gif/kataNa-No/no-min.gif'
+        break
+      case 'ハ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/ha-min.gif'
+        break
+      case 'ヒ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/hi-min.gif'
+        break
+      case 'フ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/fu-min.gif'
+        break
+      case 'ヘ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/he-min.gif'
+        break
+      case 'ホ':
+        this.imgSrc = '/assets/gif/kataHa-Ho/ho-min.gif'
+        break
       }
       this.$bvModal.show('modal-syllable-kata-tato')
     },
@@ -98,6 +168,36 @@ export default {
         break
       case 'ト':
         this.soundSrc = '/assets/sounds/sei-on/20.mp3'
+        break
+      case 'ナ':
+        this.soundSrc = '/assets/sounds/sei-on/21.mp3'
+        break
+      case 'ニ':
+        this.soundSrc = '/assets/sounds/sei-on/22.mp3'
+        break
+      case 'ヌ':
+        this.soundSrc = '/assets/sounds/sei-on/23.mp3'
+        break
+      case 'ネ':
+        this.soundSrc = '/assets/sounds/sei-on/24.mp3'
+        break
+      case 'ノ':
+        this.soundSrc = '/assets/sounds/sei-on/25.mp3'
+        break
+      case 'ハ':
+        this.soundSrc = '/assets/sounds/sei-on/26.mp3'
+        break
+      case 'ヒ':
+        this.soundSrc = '/assets/sounds/sei-on/27.mp3'
+        break
+      case 'フ':
+        this.soundSrc = '/assets/sounds/sei-on/28.mp3'
+        break
+      case 'ヘ':
+        this.soundSrc = '/assets/sounds/sei-on/29.mp3'
+        break
+      case 'ホ':
+        this.soundSrc = '/assets/sounds/sei-on/30.mp3'
         break
       }
       const audio = new Audio(this.soundSrc)
