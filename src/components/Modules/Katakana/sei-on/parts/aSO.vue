f0b004fdad516b02b135c4114de70c1615c9aac0
Zatil Imani <zatilimaniahwan@gmail.com>
2021-08-19 23:17:26 +0800

    add illustration katakanaA-SO

--- src/components/Modules/Katakana/sei-on/parts/aSO.vue
+++ src/components/Modules/Katakana/sei-on/parts/aSO.vue
@@ -1,26 +1,61 @@
 <template>
   <section class="number-area ptb_30">
+    <div class="mt-2 col-sm-6 col-md-12 col-lg-12 mb-3">
+      <b-button class="bg-primary" v-b-modal.modal-illustration-aSo
+        >Vocabulary Illustration</b-button
+      >
+      <modal :modalId="'modal-illustration-aSo'" :size="'xl'">
+        <div slot="content">
+          <b-img
+            src="/assets/img/katakana/sei-on/katakanaA-SO.jpg"
+            fluid
+            alt="Fluid image"
+          ></b-img>
+          <div v-for="illustration in vocab" :key="illustration.hiragana">
+            <b-button
+              class="btn"
+              v-bind:style="illustration.styleButton"
+              @click="play(illustration.hiragana)"
+              ><p class="syllable text-white">{{ illustration.hiragana }}</p></b-button
+            >
+          </div>
+        </div>
+      </modal>
+    </div>
     <div class="col-sm-12 col-lg-12 text-center">
       <b-row class="mt-2">
-        <b-col class="mt-2" cols="2" v-for="syllable in syllabels" :key="syllable.original">
+        <b-col
+          class="mt-2"
+          cols="2"
+          v-for="syllable in syllabels"
+          :key="syllable.original"
+        >
           <b-card class="mb-2">
             <b-card-text>
-             <h3 class="syllable">{{ syllable.original }}</h3>
-             <h5 class="mt-2 romaji"> {{ syllable.romaji }}</h5>
-               <b-button  class="bg-primary mt-3" @click="showModal(syllable.original)"><em class="mdi mdi-pencil-outline"></em></b-button>
-            <b-button  class="bg-success mt-3 ml-2" @click="play(syllable.original)"><em class="mdi mdi-account-tie-voice"></em></b-button>
+              <h3 class="syllable">{{ syllable.original }}</h3>
+              <h5 class="mt-2 romaji">{{ syllable.romaji }}</h5>
+              <b-button
+                class="bg-primary mt-3"
+                @click="showModal(syllable.original)"
+                ><em class="mdi mdi-pencil-outline"></em
+              ></b-button>
+              <b-button
+                class="bg-success mt-3 ml-2"
+                @click="play(syllable.original)"
+                ><em class="mdi mdi-account-tie-voice"></em
+              ></b-button>
             </b-card-text>
           </b-card>
         </b-col>
       </b-row>
       <modal :modalId="'modal-syllable-kata-ao'">
         <div slot="content">
-           <img
-              class="img-fluid"
-              alt="Responsive image"
-              :src="imgSrc"
-              width="100%"
-            />
+          <img
+            class="img-fluid"
+            alt="Responsive image"
+            :src="imgSrc"
+            width="100%"
+          />
         </div>
       </modal>
     </div>
@@ -94,7 +129,41 @@ export default {
           romaji: 'so'
         }
       ],
-      imgSrc: ''
+      imgSrc: '',
+      vocab: [
+        {
+          hiragana: 'ソース',
+          styleButton: {
+            position: 'absolute',
+            top: '250px',
+            left: '50px'
+          }
+        },
+        {
+          hiragana: 'キウイ',
+          styleButton: {
+            position: 'absolute',
+            top: '480px',
+            left: '90px'
+          }
+        },
+        {
+          hiragana: 'ケーキ',
+          styleButton: {
+            position: 'absolute',
+            top: '320px',
+            left: '700px'
+          }
+        },
+        {
+          hiragana: 'めがねケース',
+          styleButton: {
+            position: 'absolute',
+            top: '520px',
+            left: '890px'
+          }
+        }
+      ]
     }
   },
   methods: {
@@ -164,6 +233,18 @@ export default {
         break
       case 'コ':
         this.soundSrc = '/assets/sounds/sei-on/10.mp3'
+        break
+      case 'ソース':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_A-SO_soosu.mp3'
+        break
+      case 'キウイ':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_A-SO_kiui.mp3'
+        break
+      case 'ケーキ':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_A-SO_keeki.mp3'
+        break
+      case 'めがねケース':
+        this.soundSrc = '/assets/sounds/vocab-katakana/Katakana_A-SO_meganekeesu.mp3'
         break
       }
       const audio = new Audio(this.soundSrc)
