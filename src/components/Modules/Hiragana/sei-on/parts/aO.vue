ed9677ef1fc33d8b24901d9001d7a72d95001a62
Zatil Imani <zatilimaniahwan@gmail.com>
2021-08-19 21:00:21 +0800

    Fix sound and fix modal

--- src/components/Modules/Hiragana/sei-on/parts/aO.vue
+++ src/components/Modules/Hiragana/sei-on/parts/aO.vue
@@ -1,35 +1,60 @@
 <template>
   <section class="number-area ptb_30">
-    <div class= "mt-2 col-12 text-center" >
-        <b-button class="bg-primary" @click="modalShow= !modalShow" >Vocabulary Illustration</b-button>
-         <b-modal v-model="modalShow" size="xl" style="position:relative">
-            <b-img src="/assets/img/hiragana/sei-on/HiraganaA.jpg" fluid alt="Fluid image"></b-img>
-            <p v-for="illustration in vocab" :key="illustration.hiragana">
-              <b-button class="btn syllable" v-bind:style ="illustration.styleButton" @click="play(illustration.hiragana)">{{illustration.hiragana}}</b-button>
-            </p>
-         </b-modal>
+    <div class="mt-2 col-sm-6 col-md-12 col-lg-12 mb-3">
+      <b-button class="bg-primary" v-b-modal.modal-illustration-ao
+        >Vocabulary Illustration</b-button
+      >
+      <modal :modalId="'modal-illustration-ao'" :size="'xl'">
+        <div slot="content">
+          <b-img
+            src="/assets/img/hiragana/sei-on/HiraganaA.jpg"
+            fluid
+            alt="Fluid image"
+          ></b-img>
+        <p v-for="illustration in vocab" :key="illustration.hiragana">
+          <b-button
+            class="btn syllable"
+            v-bind:style="illustration.styleButton"
+            @click="play(illustration.hiragana)"
+            >{{ illustration.hiragana }}</b-button>
+        </p>
+        </div>
+      </modal>
     </div>
     <div class="col-sm-12 col-lg-12 text-center">
       <b-row class="mt-2">
-        <b-col class="mt-2" cols="2" v-for="syllable in syllabels" :key="syllable.original">
-            <b-card>
-              <b-card-text>
+        <b-col
+          class="mt-2"
+          cols="2"
+          v-for="syllable in syllabels"
+          :key="syllable.original"
+        >
+          <b-card>
+            <b-card-text>
               <h3 class="syllable">{{ syllable.original }}</h3>
-              <h5 class="mt-2 romaji"> {{ syllable.romaji }}</h5>
-              <b-button  class="bg-primary mt-3" @click="showModal(syllable.original)"><em class="mdi mdi-pencil-outline"></em></b-button>
-              <b-button  class="bg-success mt-3 ml-2" @click="play(syllable.original)"><em class="mdi mdi-account-tie-voice"></em></b-button>
-              </b-card-text>
-            </b-card>
+              <h5 class="mt-2 romaji">{{ syllable.romaji }}</h5>
+              <b-button
+                class="bg-primary mt-3"
+                @click="showModal(syllable.original)"
+                ><em class="mdi mdi-pencil-outline"></em
+              ></b-button>
+              <b-button
+                class="bg-success mt-3 ml-2"
+                @click="play(syllable.original)"
+                ><em class="mdi mdi-account-tie-voice"></em
+              ></b-button>
+            </b-card-text>
+          </b-card>
         </b-col>
       </b-row>
       <modal :modalId="'modal-syllable-ao'">
         <div slot="content">
-           <img
-              class="img-fluid"
-              alt="Responsive image"
-              :src="imgSrc"
-              width="100%"
-            />
+          <img
+            class="img-fluid"
+            alt="Responsive image"
+            :src="imgSrc"
+            width="100%"
+          />
         </div>
       </modal>
     </div>
@@ -125,10 +150,12 @@ export default {
         soundSrc = '/assets/sounds/sei-on/5.mp3'
         break
       case 'うえ':
-        soundSrc = '/assets/sounds/illustration-vocab/hiraAo/Hiragana_A_aoi.mp3'
+        soundSrc =
+            '/assets/sounds/illustration-vocab/hiraAo/Hiragana_A_ue.mp3'
         break
       case 'あおい':
-        soundSrc = '/assets/sounds/illustration-vocab/hiraAo/Hiragana_A_ue.mp3'
+        soundSrc =
+            '/assets/sounds/illustration-vocab/hiraAo/Hiragana_A_aoi.mp3'
         break
       }
       const audio = new Audio(soundSrc)
